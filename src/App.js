3aa105ad34f4a9a40ca4ef24d9950f940fc7bf64
tran <fidtran@gmail.com>
2020-06-11 00:00:26 -0400

    implement authO

--- src/App.js
+++ src/App.js
@@ -5,30 +5,53 @@ import ListofDecks from "./components/ListofDecks";
 import LoginPanel from "./components/login";
 import LoggedIn from "./components/loggedIn";
 import SignupPanel from "./components/signupPanel";
-import Profile from "./components/userProfile";
+// import Profile from "./components/userProfile";
 import ShowCards from "./components/cards";
 import ShowOneCard from "./components/singleCard";
 import PermanentLeftDrawer from "./components/sidebar";
 import SearchDecks from "./components/searchDeck";
-import { Route, Switch } from "react-router-dom";
 import Cards from "./components/cards";
+import NavBar from "./components/NavBar";
+import { useAuth0 } from "./react-auth0-spa";
+
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import history from "./utils/history";
+
 
 function App() {
-	return (
-		// TODO: render sidebar here from components/sidebar (?)
-		<div>
-			<Switch>
-				<Route exact path="/search" component={SearchDecks} />
-				{/* <Route exact path="/cards/:deckId?/:cardId?" component={ShowOneCard} /> */}
-				{/* <Route exact path="/cards/:id" component={ShowCards} /> */}
-				{/* <Route exact path="/profile/:id" component={Profile} /> */}
-				<Route exact path="/login" component={LoginPanel} />
-				{/* <ListofDecks /> */}
-				<Route exact path="/signup" component={SignupPanel} />
-				<Route path="/" component={LoggedIn} />
-			</Switch>
-		</div>
-	);
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="App">
+      {/* Don't forget to include the history module */}
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+      </Router>
+    </div>
+	// <div>
+	// 	<Switch>
+	// 		<Route exact path="/search" component={SearchDecks} />
+	// 		{/* <Route exact path="/cards/:deckId?/:cardId?" component={ShowOneCard} /> */}
+	// 		{/* <Route exact path="/cards/:id" component={ShowCards} /> */}
+	// 		{/* <Route exact path="/profile/:id" component={Profile} /> */}
+	// 		{/* <Route exact path="/login" component={LoginPanel} /> */}
+	// 		{/* <ListofDecks /> */}
+	// 		{/* <Route exact path="/signup" component={SignupPanel} /> */}
+	// 		<Route path="/" component={LoggedIn} />
+	// 	</Switch>
+	// </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import configureStore from "./store/configureStore";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import jwt_decode from 'jwt-decode'
+import { Auth0Provider } from "./react-auth0-spa";
+import config from "./auth_config.json";
+import history from "./utils/history";
 
 let store;
 
@@ -20,12 +23,28 @@ else {
   store = configureStore();
 }
 
+// A function that routes the user to the right place
+// after login
+const onRedirectCallback = appState => {
+  history.push(
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <Auth0Provider
+          domain={config.domain}
+          client_id={config.clientId}
+          redirect_uri={window.location.origin}
+          onRedirectCallback={onRedirectCallback}
+        >
+          <App />
+        </Auth0Provider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
